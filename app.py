01811e7f14e9cb763189883976c6c07edd73738a
Llorenç Fernandez <119510686+llorenc-fer@users.noreply.github.com>
2023-01-26 16:42:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -365,5 +365,8 @@ file_ = open("dcgan.gif", "rb")
 contents = file_.read()
 data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
-
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
 
